cc75489613ba3c6586e3f48c5b47715857677bee
Fabien SK <fab@fabsk.eu>
2012-05-01 14:04:50 +0200

    Improved UI

--- data/config.js
+++ data/config.js
@@ -1,26 +1,37 @@
 var ua = document.getElementById('ua');
 
+// delete an entry
 function onDel(evt, site) {
 	$(evt.target).parents("li").remove();
 	self.postMessage(["remove", site]);
 }
 
-ua.addEventListener("keyup", function(event) {
-  if (event.keyCode == 13) {
-	  self.postMessage(["add", ua.value]);
-  }
-    return false;
-}, true);
- 
+// init the config window
 self.on('message', function(param) {
-    document.getElementById('site').value = param[0];
-    ua.value = param[1];
-    ua.focus();
-    ua.select();
+	$( "#tabs" ).tabs();
+	// current site
+	if (param[0]==null) {
+		// not a site
+		$('#tabs').tabs("select", 1);
+		$('#tabs').tabs("option","disabled", [0]);
+	} else {
+		// callback on enter: save current site
+		ua.addEventListener("keyup", function(event) {
+		  if (event.keyCode == 13) {
+			  self.postMessage(["add", ua.value]);
+		  }
+			return false;
+		}, true);
+		document.getElementById('site').value = param[0];
+		ua.value = param[1];
+		ua.focus();
+		ua.select();
+	}
 
+	// all sites
 	var all = param[2];
     var sites = document.getElementById('sites');
-	console.log(all);
+	console.log("All:", all);
 	$.each(all, function(site, agent) {
 		console.log("Adding:", site, agent);
 		var model = $(".model")
